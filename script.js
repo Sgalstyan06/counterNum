0091133b021611c8dbeb24a29e547f6e75ec612a
Sgalstyan06 <96201224+Sgalstyan06@users.noreply.github.com>
2022-02-14 19:49:47 +0400

    change function sum on closure

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ increase.addEventListener("click", function () {
 let inp1 = document.querySelector(".input1");
 let inp2 = document.querySelector(".input2");
 let resultSum = document.querySelector(".resultOfSum");
-
 let sumBtn = document.querySelector(".sum");
- sumBtn.addEventListener("click",function(){
-    let arg1 = +inp1.value;
-    let arg2 = +inp2.value
-    resultSum.value = arg1 + arg2;
-   console.log(inp1.value);
-   
-})
 
+sumBtn.addEventListener("click", function () {
+  let arg1 = +inp1.value;
+  let arg2 = +inp2.value;
+  function sum(a) {
+    return function (b) {
+      return a + b;
+    }
+  }  
+  resultSum.value = sum(arg1)(arg2);
+});
